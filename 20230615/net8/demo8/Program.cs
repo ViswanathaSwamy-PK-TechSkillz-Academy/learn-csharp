62d1ed2fe684c58c05ef18bd7287439db05aee5d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-12 09:43:40 +0530

    Swamy/11jun work (#1)
    * Demo 1 and 2
    
    * Section 2 Demo Programs
    
    * Two New Demos
    
    * .NET 6 Program
    
    * .NET 7 Program
    
    * Demo 8

---
+++ 20230615/net8/demo8/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https:// aka.ms/ new-console-template for more information
+
+Console.ForegroundColor = ConsoleColor.Blue;
+
+Console.WriteLine(Environment.CurrentDirectory);
+Console.WriteLine(Environment.OSVersion.VersionString);
+Console.WriteLine(Environment.Version);
+Console.WriteLine(Environment.UserName);
+Console.WriteLine(Environment.MachineName);
+Console.WriteLine(Environment.ProcessorCount);
+Console.WriteLine(Environment.SystemDirectory);
+Console.WriteLine(Environment.UserDomainName);
+Console.WriteLine(Environment.UserInteractive);
+Console.WriteLine(Environment.WorkingSet);
+Console.WriteLine(Environment.Is64BitOperatingSystem);
+Console.WriteLine(Environment.Is64BitProcess);
+
+Console.ResetColor();
+
+Console.WriteLine("\n\nPress any key to exit...");
+Console.ReadKey(true);
