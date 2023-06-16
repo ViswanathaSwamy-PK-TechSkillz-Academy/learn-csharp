40d8c676e04243239ff922e6f1f142648aa5eea6
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-06-16 09:16:07 +0530

    Swamy/11jun work (#2)
    * Demo 1 and 2
    
    * Section 2 Demo Programs
    
    * Two New Demos
    
    * .NET 6 Program
    
    * .NET 7 Program
    
    * Demo 8
    
    * Updates
    
    * Code Refactoring
    
    * Code Refactoring

--- 20230615/demos/newfolder/Program.cs
+++ 20230615/demos/newfolder/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello from newfolder demo!");
+
+// To display compiler version. Please comment this after viewing the compiler version
+// #error version
