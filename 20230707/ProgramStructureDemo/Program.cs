eff3a8413e5c93ae20e86de6d276dbc799d39584
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-05 22:40:12 +0530

    Initial Version

---
+++ 20230707/ProgramStructureDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+    File: Program.cs
+    Author: Viswanatha Swamy
+    Created On: 7/7/2021 10:37:00 PM
+*/
+
+/// <summary>
+/// This is a demo program to show the structure of a C# program.
+/// </summary>
+namespace ProgramStructureDemo
+{
+    // Program is a class is the container of the program. It is the entry point of the program.
+    class Program
+    {
+
+        // Main method is the entry point of the program
+        static void Main(string[] args)
+        { /* Block of code Starts here */
+            Console.WriteLine("Hello .NET 8 World!");
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        } /* Block of code Ends here */
+    }
+}
---
+++ 20230707/ReadMe.md
@@ -0,0 +1,11 @@
+# Session 6
+
+## Few Commands
+
+```powershell
+dotnet new --list
+
+dotnet new console --help
+
+dotnet new console -o ProgramStructureDemo --use-program-main --dry-run
+```
