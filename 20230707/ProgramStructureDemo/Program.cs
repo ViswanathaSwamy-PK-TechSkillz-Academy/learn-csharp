26f17059d54841648811a8026340d7f3f9bbe841
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-06 11:36:14 +0530

    Swamy/05jul csharp session6 day1 (#4)
    * Initial Version
    
    * Few Changes
    
    * Update Program.cs
    
    * Code Refactoring
    
    * Code Refactoring

---
+++ 20230707/ProgramStructureDemo/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+    File: Program.cs
+    Author: Viswanatha Swamy
+    Created On: 7/7/2021 10:37:00 PM
+*/
+
+/// <summary>
+/// This is a demo program to show the structure of a C# program.
+/// </summary>
+namespace ProgramStructureDemo
+{
+    // Program is a class is the container of the program. It is the entry point of the program.
+    class Program
+    {
+
+        // Main method is the entry point of the program
+        static void Main(string[] args)
+        { /* Block of code Starts here */
+
+            Console.WriteLine("Hello .NET 8 World!"); /* This is a statement and it ends with semi colon. */
+
+            /* The dot operator (.) is used to access members of a namespace, type, or object. */
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+
+            // Console.ReadKey(); & Console.Readkey(); are different /* C# is a case sensitive language. */
+
+        } /* Block of code Ends here */
+    }
+}
---
+++ 20230707/ReadMe.md
@@ -0,0 +1,13 @@
+# Session 6
+
+## Few Commands
+
+```powershell
+dotnet new --list
+
+dotnet new console --help
+
+dotnet new console -o ProgramStructureDemo --use-program-main --dry-run
+
+dotnet new console -o IntVariablesDemo --dry-run
+```
