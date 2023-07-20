94ce017b868556ebfc78753e36e76b14b768e34e
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-07-20 09:36:04 +0530

    Code Refactoring

---
+++ work/20230720/A1StringsDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿using HeaderFooter;
+
+Header _header = new Header();
+Footer _footer = new Footer();
+
+_header.DisplayHeader('=', "A1 Stirngs Demo");
+
+WriteLine("Hello World!");
+WriteLine("Hello World!");
+WriteLine("Hello World!");
+WriteLine("Hello World!");
+WriteLine("Hello World!");
+WriteLine("Hello World!");
+WriteLine("Hello World!");
+WriteLine("Hello World!");
+WriteLine("Hello World!");
+
+_footer.DisplayFooter('-');
