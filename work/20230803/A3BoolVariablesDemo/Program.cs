b1a118d003ff7da955fd4b5a1867ca32f1bb73a7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-03 10:48:46 +0530

    Initial Version

--- work/20230803/A3BoolVariablesDemo/Program.cs
+++ work/20230803/A3BoolVariablesDemo/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using HeaderFooter;
+
+Header _header = new();
+Footer _footer = new();
+
+_header.DisplayHeader('=', "A2 Boolean Variables Demo");
+
+ForegroundColor = ConsoleColor.Green;
+
+WriteLine($"Char: {sizeof(bool)} byte(s) of memory.");
+
+bool isManager = false;
+WriteLine(isManager ? "Manager" : "Employee");
+
+ResetColor();
+
+_footer.DisplayFooter('-');
+
