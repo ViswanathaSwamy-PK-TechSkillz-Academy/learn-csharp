ff3f8a5e4db4a15ddb29b55c06ec5e1f3e6cf747
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-13 22:38:33 +0530

    Updates

---
+++ src/S1/ProgramStructureDemoV2/Services/Maths.cs
@@ -0,0 +1,13 @@
+namespace ProgramStructureDemoV2.Services;
+
+public class Maths
+{
+    public static int MaxValue = 100;
+
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int AddNumbersV1(int a, int b) => a + b;
+}
