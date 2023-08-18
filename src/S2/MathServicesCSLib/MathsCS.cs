fdad0ec472c5fddbd53b0c72782efd21bb03583f
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-08-18 06:05:58 +0530

    Swamy/15aug coding (#30)
    * Update S1.md
    
    * Changes
    
    * Changes
    
    * Update S2.md
    
    * Update S2.md
    
    * Changes
    
    * Update Program.cs
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes

---
+++ src/S2/MathServicesCSLib/MathsCS.cs
@@ -0,0 +1,13 @@
+﻿namespace MathServicesCSLib;
+
+public class MathsCS
+{
+    public static int MaxValue = 100;
+
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int AddNumbersV1(int a, int b) => a + b;
+}
--- src/S2/ProgramStructureDemoV3/Program.cs
+++ src/S2/ProgramStructureDemoV3/Program.cs
@@ -2,25 +2,42 @@
     Using VB.NET style namespace aliasing. We are using VB.NET Library in this C# program.
 */
 using MathServices.MathLibrary;
+using MathServicesCSLib;
 
 Console.ForegroundColor = ConsoleColor.Yellow;
 
 Console.WriteLine("Hello .NET 8 World!"); /* This is a statement and it ends with semi colon. */
 
-Console.WriteLine("Enter your name: ");
+Console.Write("Enter your name: ");
 var name = Console.ReadLine();
 Console.WriteLine($"Hello {name}!");
 
+Console.ForegroundColor = ConsoleColor.Cyan;
+
 int num1 = 10, num2 = 20; /* Variable declaration and initialization. */
-Maths maths = new(); /* Instantiation of Maths class. */
-Console.WriteLine($"{num1} + {num2} = {maths.AddNumbers(num1, num2)}");
+Maths maths = new(); /* Instantiation of Maths VB.NET Lib class. */
+Console.WriteLine($"Maths VB.NET Lib => {num1} + {num2} = {maths.AddNumbers(num1, num2)}");
 
 /* Accessing static members of a class. */
-Console.WriteLine($"{num1} + {num2} = {Maths.AddNumbersV1(num1, num2)}");
-Console.WriteLine($"Max value is {Maths.MaxValue}");
+Console.WriteLine($"Maths VB.NET Lib Static Method => {num1} + {num2} = {Maths.AddNumbersV1(num1, num2)}");
+Console.WriteLine($"Maths VB.NET Lib Static Member => Max value: {Maths.MaxValue}");
 
-int value = Maths.MaxValue;
-Console.WriteLine($"Max value is {value} and it is C# Variable.");
+Console.WriteLine($"Maths VB.NET Lib Static Member => Maths.MaxValue: {Maths.MaxValue.GetType()}.");
+int maxValue = Maths.MaxValue;
+Console.WriteLine($"(C# Variable) Max value: {maxValue} and its type {maxValue.GetType()}.");
+
+Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+MathsCS mathsCs = new(); /* Instantiation of Maths C# Lib class. */
+Console.WriteLine($"Maths C# Lib => {num1} + {num2} = {mathsCs.AddNumbers(num1, num2)}");
+
+/* Accessing static members of a class. */
+Console.WriteLine($"Maths C# Lib Static Method => {num1} + {num2} = {MathsCS.AddNumbersV1(num1, num2)}");
+Console.WriteLine($"Maths C# Lib Static Member => Max value: {MathsCS.MaxValue}");
+
+Console.WriteLine($"Maths C# Lib Static Member => Maths.MaxValue: {MathsCS.MaxValue.GetType()}.");
+maxValue = MathsCS.MaxValue;
+Console.WriteLine($"(C# Variable) Max value: {maxValue} and its type {maxValue.GetType()}.");
 
 Console.ResetColor();
 
