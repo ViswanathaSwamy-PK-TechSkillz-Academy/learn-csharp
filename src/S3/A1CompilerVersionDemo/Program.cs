186dc8b37d46ba4267e0ec1870a679cbaec49819
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-09-06 07:14:34 +0530

    Swamy/05sep work (#39)
    * Another Demo
    
    * Changes
    
    * Update S3.md
    
    * Update Program.cs

---
+++ src/S3/A1CompilerVersionDemo/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+    Description: This program will display the compiler version (4.8.0-1.23374.10 (520e2f10)) and Language version (default (11.0)).
+*/
+
+Console.WriteLine("Hello Compiler Version Demo World!");
+
+// Uncomment this line to see the compiler version
+// #error version
