54ddd643b88e991f0761733cdf6c106e2fe134ad
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-08 00:06:25 +0530

    Swamy/07oct work (#49)
    * Documentation changes
    
    * Documentation Updates
    
    * New Project
    
    * Documentation
    
    * Program and Documentation Updates
    
    * Changes
    
    * .NET 7 Project
    
    * New Project
    
    * Update Program.cs
    
    * Program Updates
    
    * Built In Data Types Demo

---
+++ src/S4/BuildInTypesDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using HeaderFooter;
+
+Header _header = new();
+Footer _footer = new();
+
+_header.DisplayHeader('=', "Built In Types Demo");
+
+ForegroundColor = ConsoleColor.DarkCyan;
+
+WriteLine($"Byte: {sizeof(byte)} byte(s) of memory. Its Min and Max Values are {byte.MinValue} and {byte.MaxValue}");
+WriteLine($"SByte: {sizeof(sbyte)} byte(s) of memory. Its Min and Max Values are {sbyte.MinValue} and {sbyte.MaxValue}");
+WriteLine($"Short: {sizeof(short)} byte(s) of memory. Its Min and Max Values are {short.MinValue} and {short.MaxValue}");
+WriteLine($"UShort: {sizeof(ushort)} byte(s) of memory. Its Min and Max Values are {ushort.MinValue} and {ushort.MaxValue}");
+WriteLine($"Int: {sizeof(int)} byte(s) of memory. Its Min and Max Values are {int.MinValue} and {int.MaxValue}");
+WriteLine($"UInt: {sizeof(uint)} byte(s) of memory. Its Min and Max Values are {uint.MinValue} and {uint.MaxValue}");
+WriteLine($"Long: {sizeof(long)} byte(s) of memory. Its Min and Max Values are {long.MinValue} and {long.MaxValue}");
+WriteLine($"ULong: {sizeof(ulong)} byte(s) of memory. Its Min and Max Values are {ulong.MinValue} and {ulong.MaxValue}");
+
+ForegroundColor = ConsoleColor.DarkYellow;
+WriteLine($"\nFloat: {sizeof(float)} byte(s) of memory. Its Min and Max Values are {float.MinValue} and {float.MaxValue}");
+WriteLine($"Double: {sizeof(double)} byte(s) of memory. Its Min and Max Values are {double.MinValue} and {double.MaxValue}");
+WriteLine($"Decimal: {sizeof(decimal)} byte(s) of memory. Its Min and Max Values are {decimal.MinValue} and {decimal.MaxValue}");
+
+ForegroundColor = ConsoleColor.DarkGreen;
+WriteLine($"\nChar: {sizeof(char)} byte(s) of memory. Its Min and Max Values are {(int)char.MinValue} and {(int)char.MaxValue}");
+
+ForegroundColor = ConsoleColor.Cyan;
+WriteLine($"\nBool: {sizeof(bool)} byte(s) of memory. Its Min and Max Values are {bool.FalseString} and {bool.TrueString}");
+
+
+
+// Assigning Values
+int age = 18;
+char gender = 'M';
+string userName = "Sri Varu";
+bool isManager = true;
+decimal salary = 100000.00M;
+double bonus = 10000.00;
+float tax = 0.10F;
+const double interestRate = 0.07;
+
+ForegroundColor = ConsoleColor.Blue;
+// Displaying Values
+WriteLine($"\nAge: {age}");
+WriteLine($"Gender: {gender}");
+WriteLine($"User Name: {userName}");
+WriteLine($"Is Manager: {isManager}");
+WriteLine($"Salary: {salary}");
+WriteLine($"Bonus: {bonus}");
+WriteLine($"Tax: {tax}");
+WriteLine($"Interest Rate: {interestRate}");
+
+_footer.DisplayFooter('-');
