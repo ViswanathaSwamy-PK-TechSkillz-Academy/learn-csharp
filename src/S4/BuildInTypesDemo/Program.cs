04f3d843c53bd4544f8572e29a32a6cd90d636a7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-07 23:24:53 +0530

    Program Updates

---
+++ src/S4/BuildInTypesDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using HeaderFooter;
+
+Header _header = new();
+Footer _footer = new();
+
+_header.DisplayHeader('=', "Built In Types Demo");
+
+ForegroundColor = ConsoleColor.DarkCyan;
+
+// Default Values
+int age;
+char gender;
+string userName;
+bool isManager;
+decimal salary;
+double bonus;
+float tax;
+
+// Assigning Values
+age = 18;
+gender = 'M';
+userName = "John Doe";
+isManager = true;
+salary = 100000.00M;
+bonus = 10000.00;
+tax = 0.10F;
+
+// Displaying Values
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Gender: {gender}");
+Console.WriteLine($"User Name: {userName}");
+Console.WriteLine($"Is Manager: {isManager}");
+Console.WriteLine($"Salary: {salary}");
+Console.WriteLine($"Bonus: {bonus}");
+Console.WriteLine($"Tax: {tax}");
+
+_footer.DisplayFooter('-');
