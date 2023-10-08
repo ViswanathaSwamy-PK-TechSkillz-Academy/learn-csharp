9d6a57178f8e9f3ac88f4683efd6039d6bd4047d
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-10-08 11:26:56 +0530

    Swamy/08oct work (#50)
    * Update S4.md
    
    * Changes
    
    * Added New Method

---
+++ src/S4/ImplicitlyTypedDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using HeaderFooter;
+
+Header _header = new();
+Footer _footer = new();
+
+_header.DisplayHeader('=', "Implicit Types Demo");
+
+
+
+// Assigning Values
+int age = 18;
+char gender = 'M';
+string userName = "Sri Varu";
+bool isManager = true;
+decimal salary = 100000.00M;
+double bonus = 10000.00;
+float tax = 0.10F;
+const double interestRate = 0.07;
+
+ForegroundColor = ConsoleColor.DarkCyan;
+// Displaying Values
+WriteLine($"\nAge: {age}");
+WriteLine($"Gender: {gender}");
+WriteLine($"User Name: {userName}");
+WriteLine($"Is Manager: {isManager}");
+WriteLine($"Salary: {salary}");
+WriteLine($"Bonus: {bonus}");
+WriteLine($"Tax: {tax}");
+WriteLine($"Interest Rate: {interestRate}");
+
+var age1 = 18;
+var gender1 = 'M';
+var userName1 = "Sri Varu";
+var isManager1 = true;
+var salary1 = 100000.00M;
+var bonus1 = 10000.00;
+var tax1 = 0.10F;
+
+ForegroundColor = ConsoleColor.DarkGreen;
+// Displaying Values
+WriteLine($"\nAge: {age1}");
+WriteLine($"Gender: {gender1}");
+WriteLine($"User Name: {userName1}");
+WriteLine($"Is Manager: {isManager1}");
+WriteLine($"Salary: {salary1}");
+WriteLine($"Bonus: {bonus1}");
+WriteLine($"Tax: {tax1}");
+
+_footer.DisplayFooter('-');
